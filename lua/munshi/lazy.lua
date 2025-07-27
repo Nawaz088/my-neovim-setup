f7b3aadf5c0ddd4b7650cfef4c7e9bea93a5854a
Nawaz088 <abdulmuqeetnawaz@gmail.com>
2025-07-27 15:57:46 +0530

    some of the configs are not working change them

--- lua/munshi/lazy.lua
+++ lua/munshi/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("munshi.plugins")
+require("lazy").setup({ { import = "munshi.plugins" }, { import = "munshi.plugins.lsp" } }, {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+ },
+})
